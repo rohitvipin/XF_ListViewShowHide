061564b5871d6153fdea2b23692be1662090b269
Rohit Vipin Mathews <rmathews@agvance.net>
2017-03-20 11:01:47 -0500

    adding cust view cell

---
+++ ListViewShowHide/ListViewShowHide/RedrawViewCell.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace ListViewShowHide
+{
+    public class RedrawViewCell : ViewCell
+    {
+        public static readonly BindableProperty IsRedrawRequiredProperty = BindableProperty.Create(nameof(IsRedrawRequired), typeof(bool), typeof(RedrawViewCell), false, propertyChanged: IsRedrawRequired_Changed);
+
+        private static void IsRedrawRequired_Changed(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            if (oldvalue == newvalue)
+            {
+                return;
+            }
+            if (newvalue != null && (bool)newvalue)
+            {
+                (bindable as RedrawViewCell)?.ForceUpdateSize();
+            }
+        }
+
+        public bool IsRedrawRequired
+        {
+            get { return (bool)GetValue(IsRedrawRequiredProperty); }
+            set { SetValue(IsRedrawRequiredProperty, value); }
+        }
+    }
+}
